e24a3b032a532fdf086599bbebb5222ad86f18b6
JogindraSingh <jogindra85@gmail.com>
2025-06-19 19:16:54 +0530

    Add fare-logic.js

---
+++ fare-logic.js
@@ -0,0 +1,7 @@
+const fareRules = [
+  { service: "Ola", type: "Auto", base: 30, perKm: 6 },
+  { service: "Uber", type: "Sedan", base: 50, perKm: 13 },
+  { service: "Rapido", type: "Bike", base: 20, perKm: 4 },
+  { service: "Ola", type: "Mini", base: 45, perKm: 11 },
+  { service: "Uber", type: "SUV", base: 60, perKm: 15 }
+];
